ef9c0445d2c90b3c107db013d1118a1c1fb96bcc
Aleksey Doludin <alexei.doludin@yandex.ru>
2025-07-04 16:45:07 +0300

    Создание автотестов

--- tests.py
+++ tests.py
@@ -1,24 +1,93 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
+    @pytest.fixture
+    def collector(self):
+        """Фикстура для чистого коллектора книг"""
+        return BooksCollector()
+    
+    @pytest.fixture
+    def collector_with_books(self):
+        """Фикстура с подготовленными данными:
+        - 2 книги с разными жанрами
+        - 1 книга в избранном"""
+        collector = BooksCollector()  # Создаем новый экземпляр
+        # Добавляем тестовые данные
+        collector.add_new_book("Книга 1")
+        collector.add_new_book("Книга 2")
+        collector.set_book_genre("Книга 1", "Фантастика")
+        collector.set_book_genre("Книга 2", "Комедии")
+        collector.add_book_in_favorites("Книга 1")
+        return collector
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    # Базовые тесты
+    def test_add_new_book_valid_name(self, collector):
+        collector.add_new_book('Том Сойер')
+        assert 'Том Сойер' in collector.get_books_genre()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_invalid_name_too_short(self, collector):
+        collector.add_new_book('')
+        assert '' not in collector.get_books_genre()
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_invalid_name_too_long(self, collector):
+        long_name = 'x' * 41
+        collector.add_new_book(long_name)
+        assert long_name not in collector.get_books_genre()
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_duplicate(self, collector):
+        collector.add_new_book('Том Сойер')
+        collector.add_new_book('Том Сойер')
+        assert len(collector.get_books_genre()) == 1
+
+    def test_added_book_has_no_genre(self, collector):
+        collector.add_new_book('Том Сойер')
+        assert collector.get_book_genre('Том Сойер') == ''
+
+    # Тесты работы с жанрами
+    def test_set_book_genre_valid(self, collector):
+        collector.add_new_book('Том Сойер')
+        collector.set_book_genre('Том Сойер', 'Фантастика')
+        assert collector.get_book_genre('Том Сойер') == 'Фантастика'
+
+    def test_set_book_genre_invalid_genre(self, collector):
+        collector.add_new_book('Том Сойер')
+        collector.set_book_genre('Том Сойер', 'Фэнтези')
+        assert collector.get_book_genre('Том Сойер') == ''
+
+    def test_set_book_genre_nonexistent_book(self, collector):
+        collector.set_book_genre('Несуществующая книга', 'Фантастика')
+        assert collector.get_book_genre('Несуществующая книга') is None
+
+    # Тесты с подготовленными данными
+    def test_get_books_with_specific_genre(self, collector_with_books):
+        books = collector_with_books.get_books_with_specific_genre('Фантастика')
+        assert 'Книга 1' in books
+        assert len(books) == 1
+
+    def test_get_books_for_children(self, collector_with_books):
+        children_books = collector_with_books.get_books_for_children()
+        assert 'Книга 1' in children_books  # "Фантастика" - детский жанр
+        assert 'Книга 2' in children_books  # "Комедии" - детский жанр
+        assert len(children_books) == 2
+
+    # Тесты работы с избранным
+    def test_add_book_in_favorites(self, collector):
+        collector.add_new_book('Том Сойер')
+        collector.add_book_in_favorites('Том Сойер')
+        assert 'Том Сойер' in collector.get_list_of_favorites_books()
+
+    def test_add_book_in_favorites_twice(self, collector):
+        collector.add_new_book('Том Сойер')
+        collector.add_book_in_favorites('Том Сойер')
+        collector.add_book_in_favorites('Том Сойер')
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites(self, collector_with_books):
+        collector_with_books.delete_book_from_favorites("Книга 1")
+        assert "Книга 1" not in collector_with_books.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books(self, collector_with_books):
+        favorites = collector_with_books.get_list_of_favorites_books()
+        assert 'Книга 1' in favorites
+        assert len(favorites) == 1
